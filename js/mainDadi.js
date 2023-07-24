c87d444c84860d9dd6d60751c4c0b30883919972
TommasoBertossi <tommasobertossi@gmail.com>
2023-07-24 15:40:54 +0200

    fix dadi js

--- js/mainDadi.js
+++ js/mainDadi.js
@@ -1,14 +1,20 @@
-let randomUser = Math.floor(Math.random() * 6 + 1);
-const userThrow = randomUser;
-let randomPc = Math.floor(Math.random() * 6 + 1);
-const pcThrow = randomPc;
+// let randomUser = Math.floor(Math.random() * 6 + 1);
+let userThrow = Math.floor(Math.random() * 6 + 1);
+// let randomPc = Math.floor(Math.random() * 6 + 1);
+let pcThrow = Math.floor(Math.random() * 6 + 1);
 let winnerMessage;
+console.log(userThrow, pcThrow);
 
-
+while (userThrow == pcThrow) {
+    winnerMessage = "Facciamo un altro giro"
+    document.getElementById("message").innerHTML = winnerMessage;
+    userThrow = Math.floor(Math.random() * 6 + 1);
+    pcThrow = Math.floor(Math.random() * 6 + 1);
+}
 
 if (userThrow > pcThrow) {
-    winnerMessage = "Ha vinto l'umano"
-else if (userThrow < pcThrow)
-    winnerMessage = "Ha vinto la macchina"
+    winnerMessage = "Ha vinto l'umano";
+    }else {
+    winnerMessage = "Ha vinto la macchina";
 }
 document.getElementById("message").innerHTML = winnerMessage;
